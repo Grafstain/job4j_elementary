428ba2c9879249f08d9fc4a8d0e599afb1779594
Grafstain <grafstain@gmail.com>
2023-06-02 10:13:28 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("1 x " + i + " = " + i);
+            i++;
+
+        }
+    }
+}
